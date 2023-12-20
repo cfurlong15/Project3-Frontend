5aac76f7446d599f5519851ea9c91a3c0c7a00d6
Conor Furlong <cfurlong15@gmail.com>
2023-12-20 00:05:31 -0500

    code conditional rendering

--- src/pages/WeatherForm/WeatherForm.jsx
+++ src/pages/WeatherForm/WeatherForm.jsx
@@ -1,61 +1,81 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 //import { useParams } from "react-router"
 //import axios from 'axios'
 
 function WeatherForm() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [searched, setSearched] = useState(false);
+  const apiKey = import.meta.env.VITE_API_KEY;
+  //const {city} = useParams()
 
-    const [ weather, setWeather ] = useState(null)
-    const [ city, setCity ] = useState("")
-    const apiKey = import.meta.env.VITE_API_KEY
-    //const {city} = useParams()
-    
-
-    async function handleRequest() {
-        try {
-            
-            
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`)
-            const weatherData = await response.json()
-            //setCity(weatherData)
-            setWeather(weatherData)
-            console.log(weatherData)
-        }catch(err) {
-            console.log(err)
-        }
+  async function handleRequest() {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
+      );
+      const weatherData = await response.json();
+      //setCity(weatherData)
+      setWeather(weatherData);
+      console.log(weatherData);
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    useEffect(() => {
-        handleRequest()
-    }, [])
+  useEffect(() => {
+    handleRequest();
+  }, []);
 
-    const handleChange = (evt) => {
-        setCity(evt.target.value)
-    }
+  const handleChange = (evt) => {
+    setCity(evt.target.value);
+  };
 
-    const handleSearch = (evt) => {
-        evt.preventDefault()
-        handleRequest()
-    }
+  const handleSearch = (evt) => {
+    evt.preventDefault();
+    handleRequest();
+    setSearched(true);
+  };
+  console.log(weather);
 
-
+  if (searched) {
     return (
+      <div>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            value={city}
+            onChange={handleChange}
+            placeholder="Enter City"
+          />
+          <button type="submit">Search</button>
+        </form>
+
         <div>
-            <form onSubmit={handleSearch}>
-                <input type="text" value={city} onChange={handleChange} placeholder="Enter City" />
-                <button type="submit">Search</button>
-            </form>
-            {weather ? (
-                <div>
-                    <h2>{weather.name}</h2>
-                    <h2>{weather?.main?.temp}</h2>
-                    <p>Low: {weather?.main?.temp_min}</p>
-                    <p>High: {weather?.main?.temp_max}</p>
-                </div>
-            ) : (
-                <p>Loading...</p>
-            )}
+          <h2>{weather.name}</h2>
+          <h2>{Math.round(weather?.main?.temp)}&#x2109;</h2>
+          <p>Low: {Math.round(weather?.main?.temp_min)}&#x2109;</p>
+          <p>High: {Math.round(weather?.main?.temp_max)}&#x2109;</p>
         </div>
-    )
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            value={city}
+            onChange={handleChange}
+            placeholder="Enter City"
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        <p>Search for your city!</p>
+      </div>
+    );
+  }
 }
 
-export default WeatherForm
+export default WeatherForm;
